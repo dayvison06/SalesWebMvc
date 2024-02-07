7a88fa358431dccf2958d990893e7d94dba6dc7d
Dayvison Rocha <dayvison.rocha@hotmail.com>
2024-02-07 14:03:30 -0300

    First controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,9 +17,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Aluno"] = "Dayvison R Pereira";
 
-            return View();
+            return Redirect("https://www.youtube.com/");
         }
 
         public IActionResult Contact()
